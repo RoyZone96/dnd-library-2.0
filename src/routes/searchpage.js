c170792ed3e2dc8c6753a0dded98f97c8933363b
Matthew Wiewel <mjvwiewel@gmail.com>
2023-11-19 16:09:16 -0600

    basic start

--- src/routes/searchpage.js
+++ src/routes/searchpage.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React from 'react';
+import SearchNav from './searchNav';
+import { useState } from 'react';
 
 
-export default function searchPage(){
+export default function SearchPage(){
+
+    let [searchQuery, setSearchQuery] = useState('enter your search here, weary adventurer')
+
     return(
         <div>
-            
+            <SearchNav />
+
+            <label>Search Query: </label>
+            <input type='text' onChange={(event) => setSearchQuery(event.target.value)} />
+            <p>Value of searchQuery: {searchQuery}</p>
+
         </div>
     )
 }
