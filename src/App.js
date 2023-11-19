caf3e358ef0c967b048fd9098d81a87e7e778199
Matthew Wiewel <mjvwiewel@gmail.com>
2023-11-19 16:23:18 -0600

    fixed imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import React, {useState} from 'react'
 import Landing from './routes/landing.js';
-import searchPage from './routes/searchpage.js';
-import TopNav from './components/topnav.js';
 import './App.css';
 import SearchNav from './components/searchnav.js';
 import TopNav from './components/topnav.js';
@@ -17,6 +15,9 @@ function App() {
         <TopNav />
         <Routes>
           <Route exact path='/' Component={Landing}/>
+          <Route exact path='/searchPage' Component={SearchPage}/>
+          <Route exact path='/searchNav' Component={SearchNav}/>
+          <Route exact path='/topNav' Component={TopNav}/>
         </Routes>
       </main>
       </BrowserRouter>
