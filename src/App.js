3a3f13e7d2accc92f972b196db6b1d55cbda0a0d
Matthew Wiewel <mjvwiewel@gmail.com>
2023-11-19 15:53:25 -0600

    added navbar, added routes for new components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import React, {useState} from 'react'
 import Landing from './routes/landing.js';
 import './App.css';
+import SearchPage from './routes/searchPage.js';
+import SearchNav from './routes/searchNav.js';
+import TopNav from './routes/topNav.js';
 
 function App() {
   return(
     <div>
       <BrowserRouter>
       <main>
+        <TopNav />
         <Routes>
           <Route exact path='/' Component={Landing}/>
+          <Route exact path='/searchPage' Component={SearchPage}/>
+          <Route exact path='/searchNav' Component={SearchNav}/>
+          <Route exact path='/topNav' Component={TopNav}/>
         </Routes>
       </main>
       </BrowserRouter>
