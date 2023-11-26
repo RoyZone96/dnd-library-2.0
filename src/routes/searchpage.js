6e366866929799e9a7a56d72a9c30fd4c05ce18e
Matthew Wiewel <mjvwiewel@gmail.com>
2023-11-26 16:54:38 -0600

    changed to the brice-roy version

--- src/routes/searchpage.js
+++ src/routes/searchpage.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { useState } from 'react';
-import SearchNav from '../components/searchnav';
+import React from "react";
+import { useState } from "react";
+import SearchNav from "../components/searchnav";
+import racePage from "../components/racepage";
+import axios from "axios";
 
+export default function SearchPage() {
+  let [searchQuery, setSearchQuery] = useState(
+    "enter your search here, weary adventurer"
+  );
 
-export default function SearchPage(){
+  const url = `https://api.open5e.com/races/`;
 
-    let [searchQuery, setSearchQuery] = useState('enter your search here, weary adventurer')
+let getSearchResult = (event) => {
+    event.preventDefault();
+    console.log('clicked')
+  
+    axios.get(url + `${searchQuery}`).then((response) => {
+        console.log(response);
+      }).catch(error => console.log(error))
+    ;
+  };
 
-    return(
-        <div>
-            <SearchNav />
+  return (
+    <div>
+      <SearchNav />
 
-            <label>Search Query: </label>
-            <input type='text' onChange={(event) => setSearchQuery(event.target.value)} />
-            <p>Value of searchQuery: {searchQuery}</p>
+      <label>Search Query: </label>
+      <input
+        type="text"
+        onChange={(event) => setSearchQuery(event.target.value)}
+      />
+      <button onClick={getSearchResult}>Search</button>
+      <p>Value of searchQuery: {searchQuery}</p>
 
-        </div>
-    )
+      <racePage raceToSearch={searchQuery} />
+
+      <div className="search-area"></div>
+    </div>
+  );
 }
